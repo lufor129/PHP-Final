a7407b990993e22676ec031aace53843215bf1f1
lufor129 <s59654655@gmail.com>
2018-05-20 16:52:09 +0800

    add part

---
+++ part/dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+    $link=mysqli_connect(
+        "localhost",
+        "root",
+        "s2007tyu",
+        "php-final"
+    )
+
+    if($link){
+
+    }else{
+        die("無法連接到伺服器");
+    }
+
+?>
---
+++ part/session.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+
+    if($_SESSION["login"]!=true){
+        header("Refresh:0;url='login.html'");
+    }
+
+
+?>
