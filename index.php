b829613b242c2a20f0925129963e124bb3f0ca7e
lufor129 <s59654655@gmail.com>
2018-05-27 00:46:17 +0800

    new head-nav

--- index.php
+++ index.php
@@ -1,29 +1,114 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>肥宅漫畫</title>
-		<meta charset = "utf-8">
-		<style type="text/css">
-			@import url("css/indexstyle.css");
-		</style>
-	</head>
-	<body style = "background-color:#050505;">
-		<?php include "comichead.php";?>
-		<center>
-			<div id = "mainchunk">
-				<font class = "bigtitle">想看漫畫，首選肥宅</font><br/><br/>
-				<font class = "dfttext">無論何時何地都可享受優質漫畫<br/>
-										還在用滿是廣告又要花大錢的線上漫畫?<br/>
-										使用肥宅，還你乾淨漂亮又快速的介面
-				</font><br/><br/><br/>
-				<button class = "bigstyle1" onClick = "location.href = 'informations.php'">了解肥宅</button>
-				<button class = "bigstyle2" onClick = "location.href = 'sign.php'">加入肥宅</button>
-			</div>
-		</center>
-		<!--footer chunk-->
-			<div id = "footer">
-				<font class = "smalltext">這只是我們上課的專題<br/>並沒有真的要架漫畫網</font>
-			</div>
-		
-	</body>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="Boostrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="Boostrap/js/bootstrap.min.js"></script>
+    <style>
+        div.carousel-item img {
+            width: 1200px;
+            height: 680px;
+        }
+
+        div#carousel-example-generic {
+            width: 80%;
+            height: 100%;
+            margin: 0 auto;
+        }
+        div.powerpoint{
+            background: #194B47;
+        }
+        div.content{
+            width:82%;
+            margin:0 auto;
+            margin-top:20px;
+            margin-bottom:20px;
+            box-shadow:2px 2px 2px 2px gray;
+        }
+    </style>
+</head>
+<body>
+    <div class="head">
+        <?php require_once "part/nav-header.php"?>
+    </div>
+    <div class="powerpoint">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" data-interval="3000">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <!--寫一個get來傳遞想看漫畫-->
+                    <a href="#">
+                        <img src="https://www.gamewikia.com/uploads/2018/05/14/cover.640.360.af24cb8d24a089cda64d45a32dae9553.jpeg">
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="http://8comic.se/wp-content/uploads/2015/09/%E9%80%B2%E6%93%8A%E7%9A%84%E5%B7%A8%E4%BA%BA-640x360.jpg">
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="https://bnetproduct-a.akamaihd.net//2d/c911f6fbe51f8dd3085d612fb43089c0-overwatch-origins-640x360-zhtw.jpg">
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="#">
+                        <img src="http://8comic.se/wp-content/uploads/2015/09/%E9%AD%94%E7%AC%9B-640x360.jpg">
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="">
+                        <img src="https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=3905512796,2920888282&fm=173&s=A033C634091272DCD49F0C4D030030FA&w=640&h=360&img.JPEG">
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="">
+                        <img src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=4294592116,563774963&fm=173&s=1720F906D06154A45479E1DF03000021&w=640&h=360&img.JPEG">
+                    </a>
+                </div>
+                <div class="carousel-item">
+                    <a href="">
+                        <img src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2983662931,863107453&fm=173&s=AF3A618598533A4F78B706830300B08F&w=640&h=360&img.JPEG">
+                    </a>
+                </div>
+            </div>
+        </div>
+            <a class="carousel-control-prev" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+    </div>
+    <hr>
+    <div class="content">
+        
+    </div>
+        <script>
+            $(function(){
+                $(".content").load("js/test.html");
+            });
+        </script>
+    </div>
+</body>
+
 </html>
---
+++ part/nav-header.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../Boostrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="../Boostrap/js/bootstrap.min.js"></script>
+    <style>
+        body{
+            font-family: 微軟正黑體;
+            font-size:20px;
+        }
+        a.navbar-brand{
+            font-size:30px;
+        }
+        div.container-fluid{
+            z-index:6000;
+            margin-bottom:71px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" style="width:100%">
+                <a class="navbar-brand" href="#">肥宅漫畫</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <?php
+                        session_start();
+                        
+                        if($_SESSION["login"]!=true){
+                            echo '<li class="nav-item">';
+                            echo '<a class="nav-link" href="#">歡迎! 訪客</a>';
+                            echo '</li>';
+                        }elseif($_SESSION["user"]=="root"){
+                            echo '<li class="nav-item">';
+                            echo '<a class="nav-link" href="admin-data.php">歡迎! 管理者</a>';
+                            echo '</li>';
+                        }else{
+                            echo '<li class="nav-item">';
+                            echo '<a class="nav-link" href="user-data.php">歡迎! '.$_SESSION["user"].'</a>';
+                            echo '</li>';
+                        }
+                    ?>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        看漫畫
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item disabled" href="#">分類</a>
+                        <a class="dropdown-item" href="#">熱血</a>
+                        <a class="dropdown-item" href="#">校園</a>
+                        <a class="dropdown-item" href="#">奇幻</a>
+                        <a class="dropdown-item" href="#">搞笑</a>
+                        <a class="dropdown-item" href="#">日常</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item disabled" href="#">排名</a>
+                        <a class="dropdown-item" href="#">點擊數</a>
+                        <a class="dropdown-item" href="#">收藏數</a>
+                        <a class="dropdown-item" href="#">最多人買</a>
+                      </div>
+                    </li>
+                    <?php
+                        if($_SESSION["login"]!=true){
+                            echo '<li class="nav-item"><a class="nav-link" href="information.php">平台說明</a></li>';
+                            echo '<li class="nav-item visitor"><a class="nav-link" href="login.php">登入</a></li>';
+                            echo '<li class="nav-item visiter"><a class="nav-link" href="register.php">註冊</a></li>';
+                        }elseif($_SESSION["user"]=="root"){
+                            echo '<li class="nav-item visitor"><a class="nav-link" href="part/logout.php">登出</a></li>';
+                        }else{
+                            echo '<li class="nav-item"><a class="nav-link" href="information.php">平台說明</a></li>';
+                            echo '<li class="nav-item visitor"><a class="nav-link" href="part/logout.php">登出</a></li>';
+                        }
+                    
+                    ?>
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">搜尋漫畫</button>
+                  </form>
+                </div>
+        </nav>
+    </div>
+</body>
+</html>
