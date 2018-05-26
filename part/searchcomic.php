efca31b401571e54ee43303bd72212b0302d6cf2
lufor129 <s59654655@gmail.com>
2018-05-26 10:01:24 +0800

    half finish comicblock (not DB connect test)

--- part/searchcomic.php
+++ part/searchcomic.php
@@ -1,8 +1,21 @@
 <?php
     require_once "dbconnect.php";
 
-    $commicname=$_GET["search"];
-    $sql-"SELECT * from commic where name='$commicname'";
+    if(isset($_GET["search"])){
+        $comicname=$_GET["search"];
+        $sql="SELECT * from commic where name='$commicname'";
+        $result=$link->query($sql);
+        if($result->row_nums>0){
+            
+        }else{
+            echo "<center><font color='red'>";
+            echo "本系統未有此漫畫<br/>";
+            echo "</font>";
+        }
+    }
+    
+
+
 
     
 
